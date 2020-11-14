4aed16e80e763606338de995fb5106f071ec3ca4
Avinash_Varma <52931707+avinashvarma531@users.noreply.github.com>
2020-11-14 10:21:27 +0530

    added comments

--- Zen/blog/models.py
+++ Zen/blog/models.py
@@ -3,12 +3,25 @@ from django.utils.timezone import now
 from ckeditor.fields import RichTextField
 
 class Tag(models.Model):
+    '''
+    Blueprint for all the tags in the blog
+    Tag model or it's objects has many-to-many relationship with Post model and it's objects
+    '''
+    
+    # properties
     name = models.CharField(max_length=15)
 
+    # behaviours
     def __str__(self):
         return self.name
 
 class Post(models.Model):
+    '''
+    Blueprint for all the posts in the blog
+    Post model or it's objects has many-to-many relationship with Tag model and it's objects
+    '''
+    
+     # properties
     title = models.CharField(max_length=100)
     description = models.TextField()
     content = RichTextField()
@@ -16,5 +29,6 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag)
     claps = models.IntegerField()
 
+    # behaviours
     def __str__(self):
         return f"{self.id} - \"{self.title}\""
