0391a19abed65412978e0efa213ff095566249a9
Avinash_Varma <52931707+avinashvarma531@users.noreply.github.com>
2020-11-14 10:12:37 +0530

    added comments and helper functions

--- Zen/blog/views.py
+++ Zen/blog/views.py
@@ -5,15 +5,21 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 
 
+# helper fns
+def get_page(req):
+    paginator = Paginator(posts, 6)
+    page_num = req.GET.get('page')
+    page_obj = paginator.get_page(page_num)
+    return page_obj
+# end helper fns
+
+# Home page
 def home(request):
     posts = Post.objects.all().order_by('-date_posted')
     tags = Tag.objects.all().order_by('name')
     popular = Post.objects.all().order_by('-claps')[:3]
-
-    paginator = Paginator(posts, 6)
-    page_num = request.GET.get('page')
-    page_obj = paginator.get_page(page_num)
-
+    page_obj = get_page(request)
+    
     context = {
         'posts': page_obj,
         'tags': tags,
@@ -21,33 +27,39 @@ def home(request):
     }
     return render(request, 'blog/post_list.html', context)
 
+# specific article page
 class PostDetailView(DetailView):
     model = Post
     context_object_name = 'post'
-    
+
+# About page
 def about_view(request):
     return render(request, 'blog/about.html')
 
+# Contact page
 def contact_view(request):
     if request.method == 'POST':
-        print(f'\n{request.POST.get("name")}\n{request.POST.get("email-id")}\n{request.POST.get("message")}\n')
-        messages.success(request, 'Thank you for your interest! You will be replied quickly')
+        name = request.POST.get("name")
+        email = request.POST.get("email-id")
+        message = request.POST.get("message")
+        print(f'\n{name}\n{email}\n{message}\n')
+        
+        messages.success(request, 'Thank you for your message! You will be replied quickly :)')
         return redirect('blog-contact')
+    
     return render(request, 'blog/contact.html')
 
+# filter by tag name
 def tag_posts(request, **kwargs):
     tag_name = kwargs['name']
     posts = Tag.objects.get(name=tag_name).post_set.all().order_by('-date_posted')
-    tags = Tag.objects.all().order_by('name')
-    popular = Post.objects.all().order_by('-claps')[:3]
-
-    paginator = Paginator(posts, 6)
-    page_num = request.GET.get('page')
-    page_obj = paginator.get_page(page_num)
+    all_tags = Tag.objects.all().order_by('name')
+    popular_posts = Post.objects.all().order_by('-claps')[:3]
+    page_obj = get_page(request)
 
     context = {
         'posts': page_obj,
-        'tags': tags,
-        'popular_posts': popular
+        'tags': all_tags,
+        'popular_posts': popular_posts
     }
     return render(request, 'blog/post_list.html', context)
