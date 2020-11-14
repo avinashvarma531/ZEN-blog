72e475b1e612df93c29a96f16d93ce7230eea631
Avinash_Varma <52931707+avinashvarma531@users.noreply.github.com>
2020-11-14 10:15:08 +0530

    added comments

--- Zen/blog/urls.py
+++ Zen/blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about_view, name='blog-about'),
-    path('contact/', views.contact_view, name='blog-contact'),
-    path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
-    path('tag/<str:name>', views.tag_posts, name='tag-posts'),
+    path('', views.home, name='blog-home'),                                         # url for home page
+    path('about/', views.about_view, name='blog-about'),                            # url for about page
+    path('contact/', views.contact_view, name='blog-contact'),                      # url for contact page
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),      # url for sepcific article
+    path('tag/<str:name>', views.tag_posts, name='tag-posts'),                      # url for tag specific posts
 ]
