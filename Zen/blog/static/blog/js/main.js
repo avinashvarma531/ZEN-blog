4643c31824ce8c139f6bce1563fb3ead3832504e
Avinash_Varma <52931707+avinashvarma531@users.noreply.github.com>
2020-11-14 10:23:36 +0530

    Update main.js

--- Zen/blog/static/blog/js/main.js
+++ Zen/blog/static/blog/js/main.js
@@ -1,20 +1,5 @@
-// gsap.registerPlugin(ScrollTrigger);
-
-// gsap.from('.post', {
-//     opacity: 0,
-//     // scale: 0.6,
-//     xPercent: -50,
-//     duration: 1,
-//     stagger: 0.2,
-//     scrollTrigger:{
-//         trigger: '.post-container',
-//         start: "top 95%",
-//         end: "bottom top",
-//         markers: true
-//     }
-// })
-const bck = document.getElementById('back-button');
-bck.addEventListener('click', ()=>{
-    console.log('Hello');
+// role of back button in article page
+const back = document.getElementById('back-button');
+back.addEventListener('click', ()=>{
     window.history.back();
 })
